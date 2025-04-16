6d7ffb692ad63c919fc1da577e76ede30bc1e368
Gabriel Bovo <gabriel.bovo@atos.net>
2025-04-16 19:58:01 -0300

    feat: subindo model

--- atividade/src/main/java/com/codifica/atividade/model/Pedido.java
+++ atividade/src/main/java/com/codifica/atividade/model/Pedido.java
@@ -2,4 +2,27 @@ package com.codifica.atividade.model;
 
 public class Pedido {
 
+    private Double peso;
+    private String transportadora;
+
+    public Pedido(Double peso, String transportadora) {
+        this.peso = peso;
+        this.transportadora = transportadora;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getTransportadora() {
+        return transportadora;
+    }
+
+    public void setTransportadora(String transportadora) {
+        this.transportadora = transportadora;
+    }
 }
